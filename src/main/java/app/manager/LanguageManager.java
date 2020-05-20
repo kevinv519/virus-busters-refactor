d7c913db04552ea0227f68a3737f37f97f8ca031
Kevin Velasquez <kevin.vela519@gmail.com>
2020-05-20 15:08:54 -0600

    Update singleton form. Added logging information. Updated to detect user preferred language.

--- src/main/java/app/manager/LanguageManager.java
+++ src/main/java/app/manager/LanguageManager.java
@@ -1,36 +1,48 @@
 package app.manager;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
  * @author Kevin
  */
 public class LanguageManager {
-    private static LanguageManager instance;
+    private static final Logger logger = LogManager.getLogger(LanguageManager.class);
     
     private ResourceBundle bundle;
     
     private LanguageManager(){
-        bundle = ResourceBundle.getBundle("locale/strings");
+        var locale = new Locale(SettingsManager.getInstance().getLanguage());
+        bundle = ResourceBundle.getBundle("locale/strings", locale);
+    }
+
+    private static class LanguageManagerHelper {
+        private static LanguageManager INSTANCE = new LanguageManager();
     }
     
     public static LanguageManager getInstance(){
-        if (instance == null){
-            instance = new LanguageManager();
-        }
-        return instance;
+        return LanguageManagerHelper.INSTANCE;
     }
-    
+
+    /**
+     * Gets the string value for the given key and the user preferred language.
+     * @param key of the string value.
+     * @return The string value, or a default message if the key cannot be found.
+     */
     public String get(String key){
         try {
             return bundle.getString(key);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return "Error";
+            logger.warn("Property with key '" + key + "' was not found.", e);
+            return bundle.getString("key_not_found");
         }
     }
     
     public void reloadLanguage(){
-        bundle = ResourceBundle.getBundle("locale/strings");
+        var locale = new Locale(SettingsManager.getInstance().getLanguage());
+        bundle = ResourceBundle.getBundle("locale/strings", locale);
     }
 }
