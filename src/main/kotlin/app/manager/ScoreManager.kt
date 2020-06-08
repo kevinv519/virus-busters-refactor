0e294e76f6c362cf9007958c345370f1fd927c3d
Kevin Velasquez <kevin.vela519@gmail.com>
2020-06-08 14:54:09 -0600

    Converted score manager to kotlin

---
+++ src/main/kotlin/app/manager/ScoreManager.kt
@@ -0,0 +1,68 @@
+package app.manager
+
+import java.io.File
+import kotlin.math.min
+
+object ScoreManager {
+    private const val LIST_SIZE = 10
+    private const val FILENAME = ".score"
+
+    private var lowerScoreIndex = 0
+
+    val scores: IntArray
+    val names: Array<String>
+
+    init {
+        scores = IntArray(LIST_SIZE) { 0 }
+        names = Array(LIST_SIZE) { "N/A" }
+        loadScores()
+    }
+
+    private fun loadScores() {
+        val file = File(FILENAME)
+        if (file.exists()) {
+            var i = 0
+            file.readLines().take(LIST_SIZE).forEach { line ->
+                val entry = line.split("=")
+                if (entry.size == 2) {
+                    names[i] = entry[0]
+                    scores[i] = entry[1].toInt()
+                    i++
+                }
+            }
+            lowerScoreIndex = if (i == LIST_SIZE) scores.lastIndex else i
+        }
+    }
+
+    private fun writeScores() {
+        var scoresText = ""
+        scores.forEachIndexed { i , score ->
+            if (score > 0) {
+                scoresText += "${names[i]}=$score\n"
+            } else return@forEachIndexed
+        }
+        File(FILENAME).writeText(scoresText.dropLast(1))
+    }
+
+    fun isHighScore(score: Int) = score >= scores[lowerScoreIndex]
+
+    fun insertNewScore(name: String, score: Int) {
+        var newScoreIndex = lowerScoreIndex
+        for (i in lowerScoreIndex downTo 0) {
+            if (score >= scores[i]) {
+                if (i < scores.lastIndex) {
+                    scores[i + 1] = scores[i]
+                    names[i + 1] = names[i]
+                    newScoreIndex = i
+                }
+            } else break
+        }
+        scores[newScoreIndex] = score
+        names[newScoreIndex] = name.substring(0, min(name.length, 5))
+        writeScores()
+
+        if (lowerScoreIndex < scores.lastIndex) {
+            lowerScoreIndex++
+        }
+    }
+}
